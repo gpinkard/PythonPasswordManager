bc77f6d2e7a8c222fb7adc0870e91e04a663d64e
gpinkard <gbrlpnkrd@gmail.com>
2018-11-25 15:35:35 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Things to do:
+1. check if there is a meta file (of the form '#META#')
+"""
+import os.path
+
+def main():
+    print('python password manager')
+
+if __name__ == "__main__":
+    main()
